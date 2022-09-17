6afe6be266f8ae871600f904df6f743335cac744
Ana_Conda <bronecanna@gmail.com>
2022-09-17 18:26:13 +0100

    fixing empty files

---
+++ README.md
@@ -0,0 +1,25 @@
+# igor extractor 
+
+
+## How to
+
+### Setup
+1. Create new folder (example: igor extractor)
+2. Create "Data" folder in igor extractor
+3. In Data folder create "input" "output" folder
+4. Put the example data in input folder 
+5. Open  https://raw.githubusercontent.com/AnnaBronec/igor_extractor/master/ibw.py
+6. Download file or copy and paste to new folder and name it ibw.py
+7. open command line and navigate to igor extractor folder
+7. run in command line `pip install -r requirments.txt` 
+8. Run in command line ` python3 ibw.py --path="Data/input/t20Soma.ibw" --joined=in_a_row `
+
+
+
+Example:
+```
+python3 ibw.py --path="Data/input/2021.10.11/t53Soma.ibw" --joined=average
+
+```
+
+
---
+++ ibw.py
@@ -0,0 +1,139 @@
+import json
+import os
+import os.path
+import math
+import numpy as np
+from pprint import pformat
+import statistics
+import click
+import pandas as pd
+from matplotlib import pyplot as plt
+from igor.binarywave import load as loadibw
+from scipy.signal import argrelextrema
+
+def get_peaks(xs):
+    df = pd.DataFrame(xs, columns=['data'])
+    n = int(len(xs) / 25)
+    df['max'] = df.iloc[argrelextrema(df.data.values, np.greater_equal,
+                        order=n)[0]]['data']
+    peaks = []
+    average = sum(xs)/len(xs)
+    for i in range(len(df['max'])):
+        if not math.isnan(df['max'][i]) and df['max'][i] > average:
+           peaks.append([i, df['max'][i]]) 
+        else:
+           df['max'][i] = math.nan
+ #   print(f"num peaks: {len(peaks)}")
+  #  plt.scatter(df.index, df['max'], c='r')
+  #  plt.plot(df.index, df['data'])
+  #  plt.show()
+    return peaks
+
+
+def extract_data(path):
+    data = loadibw(path)
+    wave = data['wave']
+    values = np.nan_to_num(wave['wData']).tolist()
+    del data['wave']['wData']
+    return (data, values)
+
+def store_data(path, data, values): 
+    path = path.replace('ibw', '')
+    path = path.replace('input', 'output')
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    lines = pformat(data).splitlines()            
+    with open(f'{path}.txt', 'w') as writer: 
+        writer.writelines([line + "\n" for line in lines])
+        # json.dump(data, writer, indent=4)
+    with open(f'{path}.json', 'w') as writer:
+        json.dump(values, writer, indent=4)
+
+def plot_data(values, total_time, path, list2=None):  
+    listxachs=np.linspace(0, total_time, len(values))
+    plt.plot(listxachs, values, linewidth=0.3, color="red")
+    if list2 is not None:
+        plt.plot(listxachs, list2, linewidth=0.3, color="red", label="last")
+    plt.xlabel("Time [minutes]",
+            family = 'serif',
+            color='black',
+            weight = 'normal',
+            size = 10,
+            labelpad = 5)
+    plt.ylabel("Voltage [mV]",
+            family = 'serif',
+            color='black',
+            weight = 'normal',
+            size = 10,
+            labelpad = 5)
+    path = path.replace('ibw','svg')
+    path = path.replace('input','output')  #input, ouput = foldernames
+    plt.savefig(f'{path}',format='svg')                 #only if you want to safe it
+  #  plt.savefig(f'{path}.eps', format='eps')
+   # plt.savefig(f'{path}.svg', format='svg')
+    plt.show()
+
+
+@click.command()
+@click.option('--path', help='specify the path and filename: "Data/<date>/<filename>"')
+@click.option('--plot', default=True, help='show plot')
+@click.option('--store', default=True, help='store data: stores complete data as txt and values as json.')
+@click.option('--joined', help='join lists')
+def run(path, plot, store, joined):
+    # Extract complete data and values 
+    data, values = extract_data(path)
+
+    print("joined: ", joined)
+    
+    stacks = len(values[0])
+    datapoints = len(values)
+    DT = 5*10 ** -5
+
+    seconds = datapoints * DT
+    time = seconds / 60
+    #time = seconds/ 20
+
+    print(f"Number of stacks: {stacks} \nDatapoints per stack: {datapoints}")
+
+    flat_lists = [ list() for x in range(stacks)]
+    for l in values:
+        for i in range(stacks):
+            flat_lists[i].append(l[i])
+    joined_lists = [] 
+    total_time=0
+    listsofaverage = []
+    for i in range(len(flat_lists)):
+        joined_lists += flat_lists[i]
+        total_time += time
+        listsofaverage.append(sum(flat_lists[i])/len(flat_lists[i]))
+
+    print ("Recording time :", total_time)
+    np_flat_lists = np.array(flat_lists)
+    for i in range(len(np_flat_lists)):
+         np_flat_lists[i] = np.array(np_flat_lists[i])
+    averages = np.mean(np_flat_lists, axis=0)
+
+
+    # Store data:
+    if store:
+        store_data(path, data, values)
+    # Plot data: 
+    if plot and joined=="stacked":    
+        plot_data(values, time, path)
+    elif plot and joined=="in_a_row":
+        plot_data(joined_lists, total_time, path)
+    elif plot and joined=="first_last":
+        plot_data(flat_lists[0], time, path, flat_lists[-1])
+    elif plot and joined=="average":
+        mid= int( 0.5 * len(flat_lists))
+       # plot_data(flat_lists[mid], time ,path)
+        maxpeak = get_peaks(average)
+        print(maxpeaks)
+        plot_data(average, time ,path)
+        plt.scatter(total_time, df['max'], c='r')
+        plt.plot(total_time, df['data'])
+        plt.show()
+
+       # plot_data(listsofaverage, time, path)
+        
+if __name__ == '__main__': 
+    run()
