d6d102724e9d9c26050708b8d054ea7d9d690bd2
fux <jan.dick@bdp.org>
2023-10-12 10:58:16 +0200

    Adds integral plotting and calculation of first amplitude

--- ibw.py
+++ ibw.py
@@ -1,6 +1,4 @@
-from sys import path
 from typing import List
-from matplotlib.font_manager import json
 import numpy as np
 import click
 from preprocessing import extract_data, convert_rows_to_columns, join_lists
@@ -8,19 +6,19 @@ from functions import get_only_peaks, alternate_min_max, get_peaks, peaks_to_dat
 from plotting import plot_data
 from matplotlib import pyplot as plt
 from scipy import integrate
-
+from integral import get_integral 
       
-def stacked(path, datapoints_per_stack: List[List[int]], time: float) -> None:
+def stacked(path: str, datapoints_per_stack: List[List[int]], time: float) -> None:
     # plt.ylim(-70, -35)
     plot_data(path, datapoints_per_stack, time)
     # print("Integral:", (-1)*np.trapez(datapoints_per_stack))
 
-def in_a_row(path, flat_lists, time: float):
+def in_a_row(path: str, flat_lists: List[List[int]], time: float):
     # Create joined lists (all stacks in a row: stack-1..n), and average stacks
     joined_lists = join_lists(flat_lists)
     return plot_data(path, joined_lists, time)
 
-def first_last(path, flat_lists, time):
+def first_last(path: str, flat_lists, time):
     plot_data(path, flat_lists[0], time, list2=flat_lists[-1])
     min_val = time[0]
     for i in [0, -1]:
@@ -29,29 +27,29 @@ def first_last(path, flat_lists, time):
    # Plot average, also mark and print peaks
 
 # elif plot and joined=="average":
-def average(path, flat_lists, time, start, step, interval):   
+def average(
+    path: str, flat_lists: List[List[int]], time: float, start: float, step: float, interval: float
+):   
+    # Calculate average
     avrg = [sum([x[i] for x in flat_lists])/len([x[i] for x in flat_lists]) for i in range(len(flat_lists[0]))]
-    with open("averages.json", "w") as f: 
-        json.dump(avrg, f)
+    # Calculate and plot integral
+    get_integral(avrg, start=start, show_plot=True)
     # Get MONOSYNAPTIC INPUT from 200ms, every 100ms, 20ms interval (first 10 peaks)
     max_peaks = get_peaks(avrg, start, step, interval, num_intervals=10, comp=np.greater_equal)
     min_peaks = get_peaks(avrg, start, step, interval, num_intervals=10, comp=np.less_equal)
     min_peaks, max_peaks = alternate_min_max(min_peaks, max_peaks)
-    plt.ylim(-90, -40)
-        # Alternative method to get DYSYNAPTIC INPUT (using all data-points):
+    # plt.ylim(-200, -45) TODO (fux): removed since view was better without
+    # Alternative method to get DYSYNAPTIC INPUT (using all data-points):
     df = peaks_to_dataframe(get_only_peaks(max_peaks), get_only_peaks(min_peaks))
     print(df)
-        # Finally: plot data
+    # Finally: plot data
     plot_data(path, avrg, time, min_peaks=min_peaks, max_peaks=max_peaks, df=df)
-        # Substract min value from each value, to get integral from baseline only (not down to zero).
-    min_val = min(avrg)
-    print("INTAGRAL: ", integrate.simps([x-min_val for x in avrg]))
 
-def average_new(flat_lists, time, start, step, interval):
+def average_new(
+    flat_lists: List[List[int]], time: float, start: float, step: float, interval: float
+):
+    # Calculate average
     avrg = [sum([x[i] for x in flat_lists])/len([x[i] for x in flat_lists]) for i in range(len(flat_lists[0]))]
-  #  with open("averages.json", "w") as f: 
-   #     json.dump(avrg, f)
-
     # Get MONOSYNAPTIC INPUT from 200ms, every 100ms, 20ms interval (first 10 peaks)
     max_peaks = get_peaks(avrg, start, step, interval, num_intervals=10, comp=np.greater_equal)
     min_peaks = get_peaks(avrg, start, step, interval, num_intervals=10, comp=np.less_equal)
@@ -63,21 +61,23 @@ def average_new(flat_lists, time, start, step, interval):
         
     #Calculate min_val before using it in integral calculation
     min_val = min(avrg)        
-
     #Calculate the integral value of the entire avrg data after the avrg is complete
     integral_value = integrate.simps([x - min_val for x in avrg], dx=step)
-
     #Create a time array for the integral data
     integral_time = np.linspace(0, time, len(avrg))
-
     #Perform element-wise addition to the avrg list
     avrg_with_min_val = [x + min_val for x in avrg]
-
     #Show the integral value as text on the plot
-    plt.text(0.7, 0.9, f'Integral Value: {integral_value:.2f}', transform=plt.gca().transAxes, fontsize=12, bbox=dict(facecolor='white', edgecolor='black', boxstyle='round'))
+    plt.text(
+        0.7, 
+        0.9, 
+        f'Integral Value: {integral_value:.2f}', 
+        transform=plt.gca().transAxes, 
+        fontsize=12, 
+        bbox=dict(facecolor='white', edgecolor='black', boxstyle='round')
+    )
 
     #Finally: plot data
-
     plt.plot(integral_time, avrg, label='Average Data')
     plt.fill_between(integral_time, min_val, avrg_with_min_val, alpha=0.3, label='Integral Area')
     plt.xlabel('Time')
@@ -90,6 +90,26 @@ def average_new(flat_lists, time, start, step, interval):
 ##here old code
     print("INTEGRAL: ", integral_value)  # Integral value at the end of the data
 
+
+# def calculate_area_under_curve(flat_lists: List[List[int]], time: float, end_time=1.25):
+#     """ 
+#     Beispielaufruf:
+#         `area_under_curve = calculate_area_under_curve(flat_lists, time)`
+#         `print("Fläche unter der Kurve bis zur Zeitachse 1.25:", area_under_curve)`
+#     """
+#     # Extrahiere den ersten Wert von flat_lists
+#     first_list = flat_lists[0]
+# 
+#     # Finde den Index des Zeitpunkts, an dem die Zeitachse 1.25 erreicht
+#     index_end_time = int(end_time / time[1])  # Annahme: Zeitintervalle sind gleichmäßig
+# 
+#     # Erzeuge einen Ausschnitt der Daten bis zur Zeitachse 1.25
+#     subset_first_list = first_list[:index_end_time + 1]
+# 
+#     # Berechne die Fläche unter der Kurve mithilfe der Trapezregel
+#     area = integrate.trapz(subset_first_list, dx=time[1])
+# 
+#     return area
 
 @click.command()
 @click.option('--path', help='specify the path and filename: "Data/<date>/<filename>"')
@@ -128,7 +148,7 @@ def run(path, plot, store, joined, start, step, interval):
         average(path, flat_lists, time, start, step, interval)
         # average_new(flat_lists, time, start, step, interval)
     elif plot and joined=="average_new":
-        average_new(path, flat_lists, time, start, step, interval)
+        average_new(flat_lists, time, start, step, interval)
 
 if __name__ == '__main__':
     run()
---
+++ integral.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt 
+from scipy.integrate import simps
+from typing import List, Tuple
+
+def get_integral(ys: List[float], start=0.2, show_plot: bool = True) -> int: 
+    """
+    Calculates the integral (area underneath the curve) in the first amplitude. 
+    The first amplitude is assumed to be at `start` (milliseconds).
+    If `show_plot` is True, also plots data.
+    """
+    xs = [i for i in range(len(ys))] # Create x-values
+    base_line = ys[0] # Use fx(0) as baseline
+
+    # Find start and end of first amplitude and create reduced dataset: 
+    start, end = __cal_start_end(ys, start=0.2, base_line=base_line)
+    xs_integral = [xs[i] for i in range(start, end)]
+    ys_integral = [ys[i] for i in range(start, end)]
+    
+    # Calculate integral
+    integral = __cal_area(xs_integral, ys_integral, base_line) 
+    print(f'Area underneath the curve (TOTAL): {integral}')
+
+    # Show plot
+    if show_plot:
+        plot_integral(ys, xs, base_line, ys_integral, xs_integral)
+    return integral
+
+def plot_integral(
+    ys: List[float], xs: List[int], base_line: float, ys_int: List[float], xs_int: List[int]
+) -> None:
+    """
+    Code plots data in `ys` and additionally shades the aread underneath the
+    curve in the shortend data set ys_int (integral)
+    """
+    plt.plot(xs, ys, label="Curve")
+    plt.fill_between(xs_int, ys_int, base_line, alpha=0.2)
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    # plt.ylim(-65,-50) TODO (fux): removed since view was better without
+    plt.legend()
+    plt.show()
+
+def __cal_area(xs: List[int], ys: List[float], base_line: float):
+    return simps(ys, xs, int(base_line))
+
+def __cal_start_end(ys: List[float], start: float, base_line: float) -> Tuple[int, int]:
+    start = int(start*len(ys)/2)
+    # Use 5 as magic number to skip first minimal differences
+    for index, val in enumerate(ys): 
+        if index > start+200 and val < base_line: 
+            return start, index
+    return start, start
